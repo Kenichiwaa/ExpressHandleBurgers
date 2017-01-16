41958c00034fef9841b5ae020fc0ad4320e16de0
Kenichi <Ninja@oldmacs-MacBook-Air.local>
2017-01-16 15:19:38 -0800

    setting up basic routes to test application. Getting error with node.modules. Maybe I have open routes that need to be connected.. basic controller, model, routes, and handlebars done

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,15 @@
 var connection = require("../config/connection.js");
+
+
+//Object for all our SQL statement functions.
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,9 +3,19 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model burger.js to use its database function.
-var burger = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes required.
 router.get("/", function(req, res) {
   res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
